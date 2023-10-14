467e306d99400df3d2385ff1ad60128c5925518b
Денис Васильев <corvus007@yandex.ru>
2023-10-14 19:34:20 +0300

    Создает функцию для проверки длины строки

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
+checkStringLength('проверяемая строка', 20); // true
+checkStringLength('проверяемая строка', 18); // true
+checkStringLength('проверяемая строка', 10); // false
