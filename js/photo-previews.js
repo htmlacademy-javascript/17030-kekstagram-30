954e052f4809b06096c017778b4a0d9cd38f2245
Денис Васильев <corvus007@yandex.ru>
2023-10-28 07:46:51 +0300

    Создает модуль миниатюр фотографий

---
+++ js/photo-previews.js
@@ -0,0 +1,23 @@
+import { createPhotos } from './data.js';
+
+const photos = createPhotos();
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesFragment = document.createDocumentFragment();
+
+photos.forEach(({ url, description, likes, comments }) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  const pictureImage = pictureElement.querySelector('.picture__img');
+  const pictureLikes = pictureElement.querySelector('.picture__likes');
+  const pictureComments = pictureElement.querySelector('.picture__comments');
+
+  pictureImage.src = url;
+  pictureImage.alt = description;
+
+  pictureLikes.textContent = likes;
+  pictureComments.textContent = comments.length;
+
+  picturesFragment.append(pictureElement);
+});
+
+picturesContainer.append(picturesFragment);
