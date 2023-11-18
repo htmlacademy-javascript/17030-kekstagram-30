8747505732aa45b4bcadbff6e66ada4a117ba7b5
Денис Васильев <corvus007@yandex.ru>
2023-11-18 22:14:22 +0300

    Создает функцию `debounce()`

--- js/util.js
+++ js/util.js
@@ -1,3 +1,12 @@
 const isEscapeKey = ({ key }) => key === 'Escape';
 
+function debounce(cb, timeoutDelay) {
+  let timeoutId;
+
+  return (...params) => {
+    clearTimeout(timeoutId);
+    setTimeout(() => cb.apply(this, params), timeoutDelay);
+  };
+}
+
 export { isEscapeKey };
